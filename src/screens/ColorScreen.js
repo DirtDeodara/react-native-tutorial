293c57942423e80dfca77bd19f0ba1300886257b
Dirt Deodara <thecrowleaps@gmail.com>
2019-12-23 18:16:22 -0800

    built colorChanger type component

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, StyleSheet, FlatList} from 'react-native';
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
 
+  return (
+    <View>
+      <Button title="Add a Color" onPress={() => {
+        setColors([...colors, randomRgb()])
+      }}/>
+      <FlatList 
+      data={colors}
+      renderItem={({ item }) => {
+        return <View style={{ marginTop: 5, height: 100, width: 100, backgroundColor: item }}></View>
+      }}
+      keyExtractor={(item) => item}
+      />
+    </View>
+  )
 };
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`
+};
+
+const styles = StyleSheet.create({
+  
+});
 
 export default ColorScreen;
