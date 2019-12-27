47b842c171c947ed11dffe6f11643d7468359dd5
Dirt Deodara <thecrowleaps@gmail.com>
2019-12-27 14:30:30 -0800

    refactored CounterScreen to use useReducer hook ... totally silly

---
+++ src/screens/CounterScreenWithUseReducerHook.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react';
+import { Text, StyleSheet, Button, View } from 'react-native';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increase_count': {
+      return { ...state, count: state.count + action.payload };
+    }
+    case 'decrease_count': {
+      return { ...state, count: state.count - action.payload };
+    }
+    default:
+      return state;
+  }
+}
+
+
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const { count } = state;
+  console.log(state);
+
+  return (
+    <View>
+      <Button title="Increase" onPress={() => dispatch({ type: 'increase_count', payload: 1 })}/>
+      <Button title="Decrease" onPress={() => dispatch({ type: 'decrease_count', payload: 1 })}/>
+      <Text>`Current Count: {count}</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default CounterScreen;
